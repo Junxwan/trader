7279d580cbdec9f096b353c89579f5ef04ac9082
Junx <hugh8845sam@gmail.com>
2022-03-22 13:56:06 +0800

    調整OP樣式

--- trader/OPS/View.xaml.cs
+++ trader/OPS/View.xaml.cs
@@ -16,6 +16,42 @@ using System.Windows.Shapes;
 
 namespace trader.OPS
 {
+    public class PageData
+    {
+        public List<OPDView> CALL { get; set; } = new List<OPDView>();
+
+        public List<OPDView> PUT { get; set; } = new List<OPDView>();
+
+        public int[] Prices { set; get; } = new int[] { };
+
+        public PageData() { }
+
+        public PageData(OPW opw)
+        {
+            List<OPD> data;
+            var _call = new List<OPDView>();
+            var _put = new List<OPDView>();
+            var prices = new int[] { };
+            (data, prices) = opw.Page();
+
+            foreach (var item in data)
+            {
+                _call.Add(new OPDView(item, OP.Type.CALL));
+            }
+
+            data.Reverse();
+
+            foreach (var item in data)
+            {
+                _put.Add(new OPDView(item, OP.Type.PUT));
+            }
+
+            this.CALL = _call;
+            this.PUT = _put;
+            this.Prices = prices;
+        }
+    }
+
     /// <summary>
     /// View.xaml 的互動邏輯
     /// </summary>
@@ -23,84 +59,35 @@ namespace trader.OPS
     {
         public event PropertyChangedEventHandler? PropertyChanged;
 
+        private PageData page = new PageData();
+        public PageData Page
+        {
+            get => page;
+            set
+            {
+                page = value;
+                OnPropertyChanged("Page");
+            }
+        }
+
         public OPManage Manage
         {
             get
             {
-                return (OPManage)GetValue(OPManageProperty);
+                return (OPManage)GetValue(ManageProperty);
             }
             set
             {
-                SetValue(OPManageProperty, value);
+                SetValue(ManageProperty, value);
             }
         }
 
-        public static readonly DependencyProperty OPManageProperty =
+        public static readonly DependencyProperty ManageProperty =
             DependencyProperty.Register("Manage", typeof(OPManage), typeof(View));
-
-        private List<OPDView> call = new();
-        public List<OPDView> CALL
-        {
-            get => call;
-            set
-            {
-                call = value;
-                OnPropertyChanged("CALL");
-            }
-        }
-
-        private List<OPDView> put = new();
-        public List<OPDView> PUT
-        {
-            get
-            {
-                return put;
-            }
-            set
-            {
-                put = value;
-                OnPropertyChanged("PUT");
-            }
-        }
-
-        //履約價
-        private int[] prices = new int[] { };
-        public int[] Prices
-        {
-            get
-            {
-                return prices;
-            }
-            set
-            {
-                prices = value;
-                OnPropertyChanged("Prices");
-            }
-        }
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            List<OPD> page;
-            var _call = new List<OPDView>();
-            var _put = new List<OPDView>();
-
-            var opw = Manage.Get((string)this.selectPeriodBox.SelectedValue);
-            (page, this.Prices) = opw.Page();
-
-            foreach (var item in page)
-            {
-                _call.Add(new OPDView(item, OP.Type.CALL));
-            }
-
-            page.Reverse();
-
-            foreach (var item in page)
-            {
-                _put.Add(new OPDView(item, OP.Type.PUT));
-            }
-
-            this.CALL = _call;
-            this.PUT = _put;
+            this.Page = new PageData(Manage.Get((string)this.selectPeriodBox.SelectedValue));
         }
 
         private void OnPropertyChanged(string propertyName)
@@ -113,9 +100,6 @@ namespace trader.OPS
 
         public View()
         {
-            this.CALL = new List<OPDView>();
-            this.PUT = new List<OPDView>();
-
             InitializeComponent();
         }
     }
