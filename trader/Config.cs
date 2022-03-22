a25bd22c4d8248b4177c1e0cadba6ab09814693e
Junx <hugh8845sam@gmail.com>
2022-03-23 00:52:42 +0800

    新增config

---
+++ trader/Config.cs
@@ -0,0 +1,48 @@
+﻿using IniParser;
+using IniParser.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trader
+{
+    public class Config
+    {
+        private string configName = "config.ini";
+
+        private string configPath = "";
+
+        private FileIniDataParser parser;
+
+        private IniData data;
+
+        public Config()
+        {
+            this.configPath = Directory.GetCurrentDirectory() + "\\" + configName;
+
+            parser = new FileIniDataParser();
+            if (!File.Exists(this.configPath))
+            {
+                var f = File.Create(this.configPath);
+                f.Dispose();
+                f.Close();
+            }
+
+            this.data = parser.ReadFile(this.configPath);
+        }
+
+        public void WriteData(string key, string value)
+        {
+            data["Data"][key] = value;
+            parser.WriteFile(this.configPath, data);
+        }
+
+        public string GetData(string key)
+        {
+            return data["Data"][key];
+        }
+    }
+}
--- trader/MainWindow.xaml.cs
+++ trader/MainWindow.xaml.cs
@@ -3,6 +3,9 @@ using System.Windows;
 using System.Windows.Forms;
 using trader.OPS;
 using trader.Futures;
+using IniParser;
+using IniParser.Model;
+using System.IO;
 
 namespace trader
 {
@@ -15,21 +18,27 @@ namespace trader
 
         private Price Futures;
 
+        private Config config;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.config = new Config();
+            this.DataPath.Text = this.config.GetData("Path");
             this.Futures = new Price(this.DataPath.Text + "\\futures");
             this.OP = new OPManage(this.DataPath.Text, this.Futures);
             this.Date.SelectedDate = DateTime.Now;
         }
 
+        //打開OP未平倉
         private void Button_OPWindow_Click(object sender, RoutedEventArgs e)
         {
             var op = new OPWindow();
             op.Show();
         }
 
-        private void Button_OpenDir_Click(object sender, RoutedEventArgs e)
+        //選擇資料目錄
+        private void Button_OpenDataDir_Click(object sender, RoutedEventArgs e)
         {
             var folder = new FolderBrowserDialog();
             var result = folder.ShowDialog();
@@ -41,7 +50,8 @@ namespace trader
             this.DataPath.Text = folder.SelectedPath;
         }
 
-        private void Button_DownloadOP_Click(object sender, RoutedEventArgs e)
+        //下載OP未平倉
+        private void Button_DownloadOPTotal_Click(object sender, RoutedEventArgs e)
         {
             if (this.OP.Download(DateTime.Parse(this.Date.Text)))
             {
@@ -53,7 +63,8 @@ namespace trader
             }
         }
 
-        private void Button_DownloadOPFutures_Click(object sender, RoutedEventArgs e)
+        //下載台指日K資料
+        private void Button_DownloadFutures_Click(object sender, RoutedEventArgs e)
         {
             if (this.Futures.Download(DateTime.Parse(this.Date.Text)))
             {
@@ -63,6 +74,13 @@ namespace trader
             {
                 System.Windows.MessageBox.Show("失敗");
             }
+        }
+
+        //保存config
+        private void Button_Config_Save_Click(object sender, RoutedEventArgs e)
+        {
+            this.config.WriteData("Path", this.DataPath.Text);
+            System.Windows.MessageBox.Show("完成");
         }
     }
 }
